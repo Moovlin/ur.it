7e7320b194341d4076f0895488d49d092a94db70
HankMD <hhs1642@rit.edu>
2016-11-12 18:13:40 -0500

    First Commit

---
+++ back/Database.py
@@ -0,0 +1,49 @@
+'''
+    Simple socket server using threads
+'''
+
+import socket
+import sys
+import threading
+import demjson
+import io
+
+def player():
+
+
+
+
+HOST = ''   # Symbolic name, meaning all available interfaces
+PORT = 8888 # Arbitrary non-privileged port
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print('Socket created')
+
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print('Bind failed.')
+    sys.exit()
+
+print('Socket bind complete')
+
+#Start listening on socket
+s.listen(10)
+print('Socket now listening')
+
+threads = []
+
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print('Connected with ' + addr[0] + ':' + str(addr[1]))
+    newThread = threading.Thread(target=player)
+    newThread.start()
+    threads.append(newThread)
+
+
+s.close()
