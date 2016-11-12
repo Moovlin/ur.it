f15f28a40e29d3aa5d884029a3ab8d244a054598
moovlin <raj2348@g.rit.edu>
2016-11-12 15:25:35 -0500

    Building directory

---
+++ back/BTAGRServ.java
@@ -0,0 +1,13 @@
+import java.net.ServerSocket;
+
+
+public class BTGARServ{
+
+	public static void main(String args[]){
+		ServerSocket mainSock = new ServerSocket();
+
+		while(mainSock.accept()){
+			
+		}
+	}
+}
